d913c31de263d21c726ff25a9325c820e8db8bb5
Ethan Rodrigues <145591098+erodri08@users.noreply.github.com>
2024-09-26 19:55:58 -0400

    Add files via upload

---
+++ Sketch2.java
@@ -0,0 +1,29 @@
+import processing.core.PApplet;
+/**
+* @author: Aurora Chen
+*/
+public class Sketch2 extends PApplet {
+	
+	
+  /**
+   * Called once at the beginning of execution, put your size all in this method
+   */
+  public void settings() {
+	// put your size call here
+    size(1600, 800);
+  }
+
+  /** 
+   * Called once at the beginning of execution.  Add initial set up
+   * values here i.e background, stroke, fill etc.
+   */
+  public void setup() {
+    background(210, 255, 173);
+  }
+
+  /**
+   * Called repeatedly, anything drawn to the screen goes here
+   */
+  
+  // define other methods down here.
+}
